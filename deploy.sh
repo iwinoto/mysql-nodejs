09a14a07512bc5b2b3215cc758fa8107c769bfa6
Iwan Winoto <iwinoto@gmail.com>
2017-03-27 14:22:30 +1100

    Assume au-syd region

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # create UPS for MySQL via secure gateway.
-cf cups albums-mysqldb -p '{"host":"cap-au-sg-prd-02.integration.ibmcloud.com", "port":"15481", "user":"user01", "password":"user01", "database":"data"}'
+cf cups albums-mysqldb -p '{"host":"<Secure Gateway Destination HOST>", "port":"<Secure Gateway Destination PORT>", "user":"user01", "password":"user01", "database":"data"}'
 
 cf push
